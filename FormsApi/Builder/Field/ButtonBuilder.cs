d74c6e663b0b6c3d7c7aaf1e7f03303a563d23b8
Chase McDonald <1chasemcd@gmail.com>
2026-02-19 20:49:30 -0700

    Fix husky

--- FormsApi/Builder/Field/ButtonBuilder.cs
+++ FormsApi/Builder/Field/ButtonBuilder.cs
@@ -19,9 +19,9 @@ public class ButtonBuilder<TModel>(
     private string GetMethodName()
     {
         if (methodCall.Body is UnaryExpression unary &&
-        unary.Operand is MethodCallExpression call &&
-        call.Object is ConstantExpression constantExpression &&
-        constantExpression.Value is MethodInfo method)
+unary.Operand is MethodCallExpression call &&
+call.Object is ConstantExpression constantExpression &&
+constantExpression.Value is MethodInfo method)
             return method.Name;
 
         throw new FormBuilderValidationException<TModel>(methodCall.ToString(), "Expression must be a method call.");
