cf4094df719b78025d6ad7fb2310426488f21278
Chase McDonald <1chasemcd@gmail.com>
2026-03-13 18:01:28 -0600

    Dont capitalize some words

--- FormsApi/Common/StringExtensions.cs
+++ FormsApi/Common/StringExtensions.cs
@@ -2,22 +2,34 @@ namespace FormsApi.Common;
 
 internal static class StringExtensions
 {
+    private static readonly HashSet<string> _notCapitalized = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "a","an","the",
+        "and","but","or","nor","for","so","yet",
+        "as","at","by","in","of","off","on","per","to","up","via",
+        "from","into","onto","upon","with","than","till","over","out"
+    };
     internal static string CamelCaseToWords(this string input)
     {
         if (string.IsNullOrEmpty(input))
             return input;
 
-        var result = new System.Text.StringBuilder();
-        result.Append(input[0]);
+        List<string> words = [""];
 
-        for (int i = 1; i < input.Length; i++)
+        for (int i = 0; i < input.Length; i++)
         {
             if (char.IsUpper(input[i]))
-                result.Append(' ');
+                words.Add("");
 
-            result.Append(input[i]);
+            words[words.Count - 1] += input[i];
         }
 
-        return result.ToString();
+        if (words.Count <= 2) return string.Join(' ', words);
+
+        // Fix capitalization on words that are not the first or last word
+        IEnumerable<string> wordsCorrected = words.Skip(1).SkipLast(1).Select(
+            word => _notCapitalized.Contains(word.ToLower()) ? word.ToLower() : word);
+
+        return words.First() + " " + string.Join(' ', wordsCorrected) + " " + words.Last();
     }
 }
