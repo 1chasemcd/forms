f4d033dedd389b90f76eab0b3b9184e558ebd7f6
Chase McDonald <1chasemcd@gmail.com>
2026-03-27 12:02:12 -0600

    Add basic grid view

---
+++ Web/src/app/view/subproperty-grid-view/subproperty-grid-view.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subproperty-grid-view',
+  imports: [],
+  templateUrl: './subproperty-grid-view.html',
+})
+export class SubpropertyGridView {}
